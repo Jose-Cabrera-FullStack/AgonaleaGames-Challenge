b9e704bf8bf1fd62bf6ea12bbc6d222baceb2fb4
Jose Cabrera <murdoc.jose.6@gmail.com>
2023-03-18 14:32:03 -0300

    feat: notes move to z axis and delete when enter in the box trigger

---
+++ Assets/Scripts/Inventory/Cards/Structure/NotesManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotesManager : MonoBehaviour
+{
+    [SerializeField] float speed = 2f;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.z -= speed * Time.deltaTime;
+        transform.position = newPosition;
+    }
+}
---
+++ Assets/Scripts/Inventory/Cards/Triggers/DeleteInstanceTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteInstanceTrigger : MonoBehaviour
+{
+    void OnTriggerEnter(Collider note)
+    {
+        Debug.Log($"entraa");
+        Destroy(note.gameObject);
+    }
+}
